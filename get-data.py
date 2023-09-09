843b05cc7666fa8092d7b96dae4a073a55c9475e
Ben Hope <benhope.itc@gmail.com>
2023-09-09 15:51:05 +0100

    Added minimum python script

--- get-data.py
+++ get-data.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == '__main__':
     if len(sys.argv) != 1:
-        print("Usage: ./app.py <file-with-path>")
+        print("Usage: ./get-data.py <file-with-path>")
         sys.exit(1)
 
     print("Data file provided: " + sys.argv[0])
