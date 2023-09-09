c3136bc7e464b182a37b467bace4c258df2e1ded
Ben Hope <benhope.itc@gmail.com>
2023-09-09 17:07:19 +0100

    Added csv read

--- get-data.py
+++ get-data.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 
 import sys
+import csv
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: ./get-data.py <file-with-path>")
         sys.exit(1)
 
-    print("Data file provided: " + sys.argv[1])
+    dataFile = sys.argv[1]
+
+    print("Data file provided: " + dataFile)
+
+    with open(dataFile) as csvfile:
+        csvReader = csv.reader(csvfile)
+        for id in csvReader:
+            print("Data ID provided: " + id[0])
+
+    
